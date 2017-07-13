f147fbc2d7611a2c2c6c1ada6b7064d145b5a28d
IVnaBo <yewave21@163.com>
2017-07-13 16:41:50 +0800

    自定义键盘。在别人的基础上 添加Swift 的使用

---
+++ KeyBoardTest/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  KeyBoardTest
+//
+//  Created by ivna.evecom on 2017/7/13.
+//  Copyright © 2017年 ivna. All rights reserved.
+//
+
+import UIKit
+
+let iPhone_W = UIScreen.main.bounds.size.width
+
+let iPhone_H = UIScreen.main.bounds.size.height
+
+
---
+++ KeyBoardTest/KeyBoardTest-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+
+#import "LDDKeyboardView.h"
+///播放声音
+#import <AudioToolbox/AudioToolbox.h>
+#import "CYRKeyboardButton.h"
